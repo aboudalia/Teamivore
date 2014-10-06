deddc88bdfca971f239759c0ee0372797a90f002
rokimomi <rokimomi@gmail.com>
2014-10-06 15:53:03 -0400

    Initial Commit

---
+++ Teamivore/src/edu/umich/teamivore/OverviewActivity.java
@@ -0,0 +1,81 @@
+package edu.umich.teamivore;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class OverviewActivity extends ActionBarActivity {
+
+	List<Map<String, String>> teamsList = new ArrayList<Map<String,String>>();
+	public final static String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_overview);
+		
+		initList();
+	    ListView teamListView = (ListView) findViewById(R.id.listView1);
+	    SimpleAdapter simpleAdpt = new SimpleAdapter(this, teamsList, android.R.layout.simple_list_item_1, new String[] {"team"}, new int[] {android.R.id.text1});
+	    teamListView.setAdapter(simpleAdpt);
+	    
+	    // React to user clicks on item
+		teamListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+		 
+		     public void onItemClick(AdapterView<?> parentAdapter, View view, int position,
+		                             long id) {
+		              
+		      
+		    	 openTeamDetail(id);
+		    	 
+		         // We know the View is a TextView so we can cast it
+		         //TextView clickedView = (TextView) view;
+		 
+		         
+		         
+		         //Toast.makeText(OverviewActivity.this, "Item with id ["+id+"] - Position ["+position+"] - Planet ["+clickedView.getText()+"]", Toast.LENGTH_SHORT).show();
+		 
+		     }
+		});
+	    
+
+	}
+
+	private void initList() {
+		teamsList.add(createTeam("team", "Team 1"));
+		teamsList.add(createTeam("team", "Team 2"));
+		teamsList.add(createTeam("team", "Team 3"));
+		teamsList.add(createTeam("team", "Team 4"));
+		teamsList.add(createTeam("team", "Team 5"));
+		teamsList.add(createTeam("team", "Team 6"));
+		teamsList.add(createTeam("team", "Team 7"));
+	}
+
+	private HashMap<String, String> createTeam(String key, String name) {
+		HashMap<String, String> team = new HashMap<String, String>();
+		team.put(key, name);
+		return team;
+	}
+	
+	
+	
+	public void openTeamDetail(long id) {
+		Intent intent = new Intent(this, TeamDetailActivity.class);
+	    String message = String.valueOf(id);
+	    intent.putExtra(EXTRA_MESSAGE, message);
+	    startActivity(intent);
+	}
+	
+}
+
---
+++ Teamivore/src/edu/umich/teamivore/TeamDetailActivity.java
@@ -0,0 +1,68 @@
+package edu.umich.teamivore;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class TeamDetailActivity extends ActionBarActivity {
+
+	List<String> teamsList = new ArrayList<>();
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_team_detail);
+		
+		initList();
+		
+		// Get the message from the intent
+	    Intent intent = getIntent();
+	    String message = intent.getStringExtra(OverviewActivity.EXTRA_MESSAGE);
+
+	    int id = (int) Long.parseLong(message);
+	    
+	    // Create the text view
+	    TextView textView = new TextView(this);
+	    textView.setTextSize(40);
+	    textView.setText(teamsList.get(id));
+
+	    // Set the text view as the activity layout
+	    setContentView(textView);
+		
+	}
+	
+	private void initList() {
+		teamsList.add("Team 1");
+		teamsList.add("Team 2");
+		teamsList.add("Team 3");
+		teamsList.add("Team 4");
+		teamsList.add("Team 5");
+		teamsList.add("Team 6");
+		teamsList.add("Team 7");
+	}
+	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.team_detail, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+}
