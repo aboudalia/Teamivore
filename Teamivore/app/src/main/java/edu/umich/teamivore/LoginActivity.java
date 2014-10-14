aae4433742ae48365c3109ffac3c929b934069b9
rokimomi <rokimomi@gmail.com>
2014-10-14 11:29:11 -0400

    moved project to android studio

---
+++ Teamivore/app/src/main/java/edu/umich/teamivore/LoginActivity.java
@@ -0,0 +1,23 @@
+package edu.umich.teamivore;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+
+public class LoginActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+    public void logIn(View view) {
+        Intent intent = new Intent(this, OverviewActivity.class);
+        startActivity(intent);
+    }
+}
